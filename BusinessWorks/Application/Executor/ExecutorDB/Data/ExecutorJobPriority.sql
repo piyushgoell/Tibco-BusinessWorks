7ab7c71145796314f73ccce44d8e83c8f6a67495
Piyush Goel <34750712+piyushgoell@users.noreply.github.com>
2024-02-08 13:11:11 +0530

    Updated ExecutorDB folder structure

--- BusinessWorks/Application/Executor/ExecutorDB/Data/ExecutorJobPriority.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Executor.ExecutorJobPriority(PriorityId, Priority)
-	VALUES 
-	(1, 'HIGH'),
-	(2, 'NORMAL'),
-	(3, 'LOW');
--- BusinessWorks/Application/Executor/ExecutorDB/Data/ExecutorJobType.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Executor.ExecutorJobType(TypeId,Type)
-	VALUES 
-	(1,'MyJob');
